1fda70e5cbad9b3b861d586bb0bdec8e650d58ae
钦烨 <admibrill@outlook.com>
2025-09-05 01:22:47 +0800

    Update launcher.cpp

--- launcher.cpp
+++ launcher.cpp
@@ -26,8 +26,8 @@ int main(int argc,char* argv[]){
     if (engine.rootObjects().isEmpty())
         return -1;
     qDebug()<<"Application Launched Successfully.";
-    UniDeskGlobals::getInstance()->startThread();
-    UniDeskComponentsData::getInstance()->startFuncs();
+    // UniDeskGlobals::getInstance()->startThread();
+    // UniDeskComponentsData::getInstance()->startFuncs();
     return app.exec();
 }
 
